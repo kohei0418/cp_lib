2d1998abd031383aa133ed966e40e5ec7316a2ff
kohei0418 <light.amp.ser@gmail.com>
2012-09-12 22:44:54 +0900

    first commit

---
+++ template.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+#include <cctype>
+#include <cassert>
+#include <cstring>
+#include <climits>
+
+using namespace std;
+
+#define FOR(k,a,b) for(typeof(a) k=(a); k < (b); k++)
+#define FORE(k,a,b) for(typeof(a) k=(a); k <= (b); k++)
+#define REP(k,a) for(int k=0; k < (a); k++)
+
+#define SZ size()
+#define ALL(c) (c).begin(), (c).end()
+#define PB push_back
+#define MP make_pair
+#define EACH(i,c) for(typeof((c).begin()) i=(c).begin(); i!=(c).end(); i++)
+#define EXIST(s,e) ((s).find(e)!=(s).end())
+
+#define dump(x) cerr << #x << ": " << (x) << endl;
+
+typedef long long ll;
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef pair<int, int> PII;
+
+const int INF = 1000 * 1000 * 1000;
+const double EPS = 1e-10;
+
+
+
+int main()
+{
+  
+  
+  return 0;
+}
