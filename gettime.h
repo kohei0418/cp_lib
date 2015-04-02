0c85dceb40eebae24954fd8bad111098373be564
kohei0418 <lignt.amp.ser@gmail.com>
2015-04-02 22:37:18 +0900

    change: tab width 2 => 4

--- gettime.h
+++
@@ -1,8 +0,0 @@
-#include <time.h>
-#include <sys/time.h>
-
-double gettime(){    
-  struct timeval tv;    
-  gettimeofday(&tv, NULL);
-  return tv.tv_sec + (double)tv.tv_usec*1e-6;
-}
---
+++ union_find.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+
+using namespace std;
+
+// Reference: http://www.prefield.com/algorithm/
+struct UnionFind {
+    vector<int> data;
+    UnionFind(int size) : data(size, -1) {}
+    bool unite(int x, int y) {
+        x = root(x); y = root(y);
+        if(x != y) {
+            if(data[y] < data[x]) swap(x, y);
+            data[x] += data[y]; data[y] = x;
+        }
+        return x != y;
+    } 
+    bool find(int x, int y) {
+        return root(x) == root(y);
+    }
+    int root(int x) {
+        return data[x] < 0? x : data[x] = root(data[x]);
+    }
+    int size(int x) {
+        return -data[root(x)];
+    }
+};
